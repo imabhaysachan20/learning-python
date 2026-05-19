388d61f2cc28f102538808a122a25ba716563c0c
abhay <imabhaysachan@gmail.com>
2026-05-19 16:19:55 +0530

    add approach 2

--- assignment/Assignment-4-18-May.py
+++ assignment/Assignment-4-18-May.py
@@ -15,8 +15,28 @@ for x in data:
 print(list(answer))
 
 #freq of each elem
+
+#approach 1
 ans = []
 for x in unique:
     ans.append([x,data.count(x)])
+
+print(ans)
+
+#approach 2
+arr2 = data.copy()
+arr2.sort()
+ans = list()
+count = 1
+for i in range(len(arr2)-1):
+    if arr2[i]==arr2[i+1]:
+        count += 1
+    else:
+        ans.append([arr2[i], count])
+        count = 1
+
+    print(i)
+
+ans.append([arr2[i+1], count])
 
 print(ans)
