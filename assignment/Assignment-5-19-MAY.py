67c7306f44b0edaf4d1f027316ac1e325a0212e4
abhay <imabhaysachan@gmail.com>
2026-05-20 07:56:17 +0530

    add 19may assignment 1

---
+++ assignment/Assignment-5-19-MAY.py
@@ -0,0 +1,41 @@
+students = {
+    "Rahul": 85,
+    "Sneha": 67,
+    "Amit": 67,
+    "John": 45
+
+}
+
+# Find topper
+max = -1e9
+ans = ""
+for x in students:
+    if(students[x]>max):
+        max = students[x]
+        ans = x
+print(ans)
+
+# Find failed students (<50)
+ans = []
+for x in students:
+    if(students[x]<50):
+        ans.append(x)
+print(ans)
+
+# Find students with same marks
+
+
+# Print grades
+for x in students:
+    print(students[x])
+
+# Merge and sort dictionary both by key and value
+
+##by key
+d1 = {"a": 1, "b": 4}
+d2 = {"c": 3, "d": 4}
+d2.update(d1)
+print(dict(sorted(d2.items())))
+## by value
+print(dict(sorted(d2.items(),key=lambda x:x[1])))
+
---
+++ assignment/Assignment-6-19-MAY.py
@@ -0,0 +1,7 @@
+cart = {
+    "Laptop": {"price": 50000, "qty": 1},
+    "Mouse": {"price": 500, "qty": 2}
+}
+
+def addProduct(product):
+    
