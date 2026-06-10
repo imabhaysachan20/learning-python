56ea2c61587cd99ab78939a75aefe811f7f91176
Abhay Sachan <imabhaysachan@gmail.com>
2026-06-10 14:33:48 +0530

    add generators

--- learning/iterator-generator.py
+++ learning/iterator-generator.py
@@ -56,26 +56,114 @@ nums = [1,2,3]
 
 ##we can make our own iterator object!
 
-class MyRange:
-    def __init__(self,start,end):
-        self.value = start
-        self.end = end
+# class MyRange:
+#     def __init__(self,start,end):
+#         self.value = start
+#         self.end = end
     
-    def __iter__(self):
-        return self
+#     def __iter__(self):
+#         return self
     
-    def __next__(self):
-        if self.value>=self.end:
-            raise StopIteration
-        current = self.value
-        self.value+=1
-        return current  
+#     def __next__(self):
+#         if self.value>=self.end:
+#             raise StopIteration
+#         current = self.value
+#         self.value+=1
+#         return current  
 
-num = MyRange(1,10)
+# num = MyRange(1,10)
 
-print(next(num))
+# print(next(num))
 
 # for n in num:
 #     print(n)
 
 #generators
+def square_numbers(nums):
+    result = []
+    for i in nums:
+        result.append(i * i)
+    return result
+
+my_nums = square_numbers([1, 2, 3, 4, 5])
+# my_nums = [x*x for x in [1,2,3,4,5]]
+
+print (my_nums)  # [1, 4, 9, 16, 25]
+
+# for num in my_nums:
+#     print num
+def square_numbers(nums):
+    for i in nums:
+        yield (i * i)
+    
+my_nums = square_numbers([1, 2, 3, 4, 5])
+# my_nums = [x*x for x in [1,2,3,4,5]]
+
+print (my_nums)  # generator object
+print (next(my_nums))
+
+#advantage
+
+my_nums = [x**x for x in range(0,100)]
+##100 elements in memory!!
+
+my_nums = (x**x for x in range(0,100))
+## just an generator object which will give us simply when we ask next thing
+
+print(my_nums)
+next(my_nums)
+
+# can be converted to list using list()
+
+my_nums_list = list(my_nums)
+
+import random
+import time
+names = ["Alice", "Bob", "Charlie", "Diana"]
+majors = ["Computer Science", "Mathematics", "Physics", "Biology"]
+def people_list(num_people):
+    result = []
+    for i in range(num_people):
+        person = {
+            'id': i,
+            'name': random.choice(names),
+            'major': random.choice(majors)
+        }
+        result.append(person)
+    return result
+
+
+def people_generator(num_people):
+    for i in range(num_people):
+        person = {
+            'id': i,
+            'name': random.choice(names),
+            'major': random.choice(majors)
+        }
+        yield person 
+
+t1 = time.perf_counter()
+
+people = people_list(1000000)
+t2 = time.perf_counter()
+
+print("time taken list", t2-t1)
+
+t1 = time.perf_counter()
+
+people = people_generator(1000000)
+t2 = time.perf_counter()
+
+print("time taken generator", t2-t1)
+
+
+##generator used to make iterator
+
+def make_range(start,end):
+    current = start
+    while current<end:
+        yield current
+        current+=1
+
+num = make_range(1,10)
+print(next(num))
